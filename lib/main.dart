08678086e1c7006e512556110c056d2ac5a7edab
Mayank <mayank.gupta250@gmail.com>
2021-12-20 13:04:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: Material(
+         child:Center(
+          child: Text("Welcome to my app"),
+         )
+    ));
+  }
+}
---
+++ lib/rachit.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        child:Center(
+          child: Text("Welcome"),
+        )
+
+      ),
+    );
+  }
+}
